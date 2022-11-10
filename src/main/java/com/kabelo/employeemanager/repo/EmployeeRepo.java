e25e89671ec0be1c12e864451bd5c3cd7cf6a22f
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-11-10 14:49:03 +0200

    Completed app

---
+++ src/main/java/com/kabelo/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.kabelo.employeemanager.repo;
+
+import com.kabelo.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
---
+++ src/main/java/com/kabelo/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.kabelo.employeemanager.service;
+
+import com.kabelo.employeemanager.exception.UserNotFoundException;
+import com.kabelo.employeemanager.model.Employee;
+import com.kabelo.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by ID "+id+" was not found"));
+    }
+}
